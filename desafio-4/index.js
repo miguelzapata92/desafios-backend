68db15fb8820e6d6ed8ebc98df1d9f506aab381e
miguelzapata92 <zptmiguel.92@gmail.com>
2022-08-17 09:27:46 -0300

    ordenando el código en carpetas

--- desafio-4/index.js
+++ desafio-4/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Router} = express;
+//const path = require('path')
 
 const app = express();
 
@@ -7,47 +7,13 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
 
-const router = Router();
-
-const listaProductos = [];
+//pp.use(express.static(path.join(__dirname, "public")))
 
 
-router.get('/', (req, res)=> {
-    //devuelve todos los productos
-    res.send(listaProductos)
-})
-
-router.get('/:id', (req, res)=> {   
-    //devuelve un producto según su id
-    const id = req.params.id
-    const producto = listaProductos.filter(e=> e.id == id)
-    res.send(producto)
-})
-
-router.post('/', (req, res)=> {
-    //recibe y agrega un producto, lo devuelve con su id
-    const data = req.body;
-
-    listaProductos.push(data);
-    res.send(listaProductos)
-})
+app.use('/api/productos', require('./routes/routes'))
 
 
-router.put('/:id', (req, res)=> {
-    //recibe y actualiza un producto segun su id
-    let idProducto = req.params.id
-    
-    res.send(`get ok ${idProducto}`);
-})
 
-router.delete('/:id', (req, res)=> {
-    //elimina un producto segun su id
-    let idProducto = req.params.id
-    listaProductos.splice(idProducto + 1, 1)
-    res.send(listaProductos);
-})
-
-app.use('/api/productos', router);
 
 
 const PORT = 8080;
---
+++ desafio-4/routes/routes.js
@@ -0,0 +1,43 @@
+
+const express= require('express')
+const router = express.Router();
+
+const listaProductos = [];
+
+
+router.get('/', (req, res)=> {
+    //devuelve todos los productos
+    res.send(listaProductos)
+})
+
+router.get('/:id', (req, res)=> {   
+    //devuelve un producto según su id
+    const id = req.params.id
+    const producto = listaProductos.filter(e=> e.id == id)
+    res.send(producto)
+})
+
+router.post('/', (req, res)=> {
+    //recibe y agrega un producto, lo devuelve con su id
+    const data = req.body;
+
+    listaProductos.push(data);
+    res.send(listaProductos)
+})
+
+
+router.put('/:id', (req, res)=> {
+    //recibe y actualiza un producto segun su id
+    let idProducto = req.params.id
+    
+    res.send(`get ok ${idProducto}`);
+})
+
+router.delete('/:id', (req, res)=> {
+    //elimina un producto segun su id
+    let idProducto = req.params.id
+    listaProductos.splice(idProducto + 1, 1)
+    res.send(listaProductos);
+})
+
+module.exports = router;
