f6ab7049067d1734da02a4d8c3513e109e328f5f
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-08-18 00:42:24 -0300

    Solucionando error en el post

--- desafio-4/index.js
+++ desafio-4/index.js
@@ -7,12 +7,11 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
 
+
 app.use(express.static(path.join(__dirname, "public")))
 
 
 app.use('/api/productos', require('./routes/routes'))
-
-
 
 
 
--- desafio-4/routes/routes.js
+++ desafio-4/routes/routes.js
@@ -20,23 +20,29 @@ router.get('/:id', (req, res)=> {
 router.post('/', (req, res)=> {
     //recibe y agrega un producto, lo devuelve con su id
     const data = req.body;
-
     listaProductos.push(data);
+    data.id = listaProductos.length;
     res.send(listaProductos)
 })
 
 
 router.put('/:id', (req, res)=> {
     //recibe y actualiza un producto segun su id
-    let idProducto = req.params.id
-    
-    res.send(`get ok ${idProducto}`);
+    const idProducto = req.params.id
+    if(!idProducto){
+        res.status(400).json({ error : 'El producto solicitado no existe'})
+    }
+    const producto = listaProductos.filter(e=> e.id == id)
+    producto.title = nombre;
+    producto.price = title;
+    producto.thumbnail = thumbnail;
+    res.send(producto);
 })
 
 router.delete('/:id', (req, res)=> {
     //elimina un producto segun su id
     let idProducto = req.params.id
-    listaProductos.splice(idProducto + 1, 1)
+    listaProductos.splice(idProducto -1 , 1)
     res.send(listaProductos);
 })
 
