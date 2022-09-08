e57964d89a67240456b0ce12017cf3ec42d263b1
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-09-08 18:59:04 -0300

    Inicializando proyecto a partir del desafío 4

---
+++ desafio7/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/productos', require('./routes/routesProducto'));
+app.use('/api/carrito', require('./routes/routesCarrito'));
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () =>{
+    console.log(`Server on port: ${PORT}`);
+})
+server.on('error', error => console.log(error));
