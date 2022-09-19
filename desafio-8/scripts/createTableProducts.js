c847ee9b264daab140781c61bb83f7212fcbf5de
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-09-19 00:05:59 -0300

    creando  el script de create tabla productos

---
+++ desafio-8/scripts/createTableProducts.js
@@ -0,0 +1,11 @@
+const { optionsSQLite3 } = require('./options/config.js');
+const knex = require('knex')(optionsSQLite3);
+
+knex.schema.createTable('products', table => {
+	table.increments('id')
+	table.string('title')
+	table.string('price')
+	table.string('thumbnail')
+})
+	.then(() => console.log('Table created'))
+	.catch(err => { console.log(err); throw err })
