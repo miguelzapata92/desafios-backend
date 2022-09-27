77b4895cc41cbfdaad5d97404d997b4a3a184d80
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2022-09-27 00:12:48 -0300

    Corrigiendo validaciones

--- desafio7/routes/routesCarrito.js
+++ desafio7/routes/routesCarrito.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import { GetCartById, AddCartProduct, DeleteProdInCartByID, DeleteCart, GetProdInCartById} from '../controllers/carritoControllers.js'; 
-
+import authValidator from '../middlewares/authValidator.js';
 const carritoRouter = express.Router();
 
 
 
 carritoRouter.get('/:id?', (req, res) => GetCartById(req, res));
 
-carritoRouter.delete('/:id', (req, res) => DeleteCart(req, res));
+carritoRouter.delete('/:id', authValidator, (req, res) => DeleteCart(req, res));
 
 carritoRouter.get('/:id?', (req, res) => GetProdInCartById(req, res));
 
-carritoRouter.post('/', (req, res) => AddCartProduct(req, res));
+carritoRouter.post('/', authValidator, (req, res) => AddCartProduct(req, res));
 
-carritoRouter.delete('/:id', (req, res) => DeleteProdInCartByID(req, res));
+carritoRouter.delete('/:id', authValidator, (req, res) => DeleteProdInCartByID(req, res));
 
 
 
